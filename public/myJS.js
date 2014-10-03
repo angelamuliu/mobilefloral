843503c382592b34adc9b1baa6d6be05dabbc39e
Angela Liu <angelamuliu@gmail.com>
2014-10-02 23:24:20 -0400

    Added a simple touch event listener

--- public/myJS.js
+++ public/myJS.js
@@ -1,18 +1,26 @@
 
 $(document).ready( function() {
 
-	document.ontouchmove = function(e){ e.preventDefault(); }
+	// Setup - Get canvas, set context to 2d mode
+	// One day context might support 3d drawing!
+	var canvas = $("#myCanvas")[0];
+	var context = canvas.getContext("2d");
 
-	// Setup - Get canvas, set context to 2d (one day in the future, 3d canvases!! but not yet.)
-	var c = document.getElementById("myCanvas");
-	var ctx = c.getContext("2d");
+	// Canvas recieves listeners
+	// Context recieves drawing information
 
-	ctx.ontouchstart = function(event){ 
-		console.lot("TEST");
+	function start(event){ 
+		console.log("TEST");
 		event.preventDefault();
-		ctx.beginPath();
-		ctx.arc(95,50,40,0,2*Math.PI);
-		ctx.stroke();
+		context.beginPath();
+		context.arc(95,50,40,0,2*Math.PI);
+		context.stroke();
 	}
 
+	// Event listener takes event, function to call, and whether or not function 'captures/consumes event'
+	// If function consumes, then it doesn't bubble up and effect parent nodes
+	canvas.addEventListener('touchstart', start, false);
+
+
 })
+
